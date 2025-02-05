84741367d473e03997bc994fc21fe7d43d5eadf9
Matteo <mattemoni@gmail.com>
2025-02-05 11:48:44 +0100

    y

---
+++ get_python_repos.py
@@ -0,0 +1,19 @@
+from github import Github
+import os
+
+ACCESS_TOKEN = open("token.txt", "r").read().strip()
+g = Github(ACCESS_TOKEN)
+print(g.get_user().login)
+repo_number = 100
+
+# Random day
+query = f"language:python created:2024-10-01..2024-10-02"
+result = g.search_repositories(query)
+print(result.totalCount)
+
+# I clone the n first repositories only
+for repo in result[:repo_number]:
+    os.system(f"git clone {repo.clone_url} repos/{repo.owner.login}/{repo.name}")
+    print(repo.clone_url)
+
+result = g.search_repositories(query)
---
+++ tokenizer.py
@@ -0,0 +1,14 @@
+from tokenizers import ByteLevelBPETokenizer
+
+paths = ["formatted_python_data.txt"]
+
+tokenizer = ByteLevelBPETokenizer()
+
+tokenizer.train(
+    files=paths,
+    vocab_size=15000,
+    min_frequency=2,
+    special_tokens=["<s>", "<pad>", "</s>", "<unk>", "<mask>", "<N>"],
+)
+
+tokenizer.save_model(".", "python_tokenizer")
