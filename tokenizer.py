4903847d3d740a696a16ebae130d553745bb7c37
Matteo <mattemoni@gmail.com>
2025-02-05 13:18:10 +0100

    Working on a proper model

--- tokenizer.py
+++ tokenizer.py
@@ -6,7 +6,7 @@ tokenizer = ByteLevelBPETokenizer()
 
 tokenizer.train(
     files=paths,
-    vocab_size=15000,
+    vocab_size=5000,
     min_frequency=2,
     special_tokens=["<s>", "<pad>", "</s>", "<unk>", "<mask>", "<N>"],
 )
