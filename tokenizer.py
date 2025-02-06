db4f0e32b7c3d39143069b53edc32b40dd49e0ca
Matteo <mattemoni@gmail.com>
2025-02-06 10:30:25 +0100

    Workin on the tokenizer

--- tokenizer.py
+++ tokenizer.py
@@ -1,14 +1,1 @@
-from tokenizers import ByteLevelBPETokenizer
 
-paths = ["formatted_python_data.txt"]
-
-tokenizer = ByteLevelBPETokenizer()
-
-tokenizer.train(
-    files=paths,
-    vocab_size=10000,
-    min_frequency=2,
-    special_tokens=["<s>", "<pad>", "</s>", "<unk>", "<mask>", "<N>"],
-)
-
-tokenizer.save_model(".", "python_tokenizer")
