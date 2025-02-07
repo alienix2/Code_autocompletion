b7314558e7639a034d86b863766619c324b5f4ea
Matteo <mattemoni@gmail.com>
2025-02-07 17:19:05 +0100

    Added top p and k

---
+++ train_transformer_k_p.py
@@ -0,0 +1,22 @@
+from transformer import train_model
+import torch
+
+data_path = "formatted_python_data.txt"
+vocab_path = "python_tokenizer-vocab.json"
+merges_path = "python_tokenizer-merges.txt"
+save_path = "python_model.pth"
+
+model, tokenizer = train_model(
+    data_path, vocab_path, merges_path, save_path, method="top_p", p=0.9
+)
+
+# Generate some text starting from 0
+device = model.token_embedding_table.weight.device
+print(
+    tokenizer.decode(
+        model.generate(
+            idx=torch.zeros((1, 1), dtype=torch.long, device=device), max_new_tokens=255
+        )[0].tolist(),
+        skip_special_tokens=True,
+    )
+)
